b4b42d59c78d8fe825a30988de4c3deb1629f2d6
Pedrommano <160624863+Pedrommano@users.noreply.github.com>
2025-06-08 22:41:00 -0300

    Update address.dart

--- lib/models/address.dart
+++ lib/models/address.dart
@@ -1,12 +1,41 @@
 import 'package:json_annotation/json_annotation.dart';
 part 'address.g.dart';
 
-@JsonSerializable()
-class Address {
-  Address(this.street, this.city);
-  String street;
-  String city;
+class Endereco {
+  final String? id;
+  final String rua;
+  final String numero;
+  final String cidade;
+  final String estado;
+  final String cep;
 
-  factory Address.fromJson(Map<String, dynamic> json) => _$AddressFromJson(json);
-  Map<String, dynamic> toJson() => _$AddressToJson(this);
+  Endereco({
+    this.id,
+    required this.rua,
+    required this.numero,
+    required this.cidade,
+    required this.estado,
+    required this.cep,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'rua': rua,
+      'numero': numero,
+      'cidade': cidade,
+      'estado': estado,
+      'cep': cep,
+    };
+  }
+
+  factory Endereco.fromMap(String id, Map<String, dynamic> map) {
+    return Endereco(
+      id: id,
+      rua: map['rua'] ?? '',
+      numero: map['numero'] ?? '',
+      cidade: map['cidade'] ?? '',
+      estado: map['estado'] ?? '',
+      cep: map['cep'] ?? '',
+    );
+  }
 }
